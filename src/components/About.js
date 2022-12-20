bc1066f10ae5fdbc7ab2ae0cb5253074a0ea3b2a
Andrew G. Kagotho <86290395+AndrewKagotho@users.noreply.github.com>
2022-12-20 14:19:42 +0300

    Further dev

--- src/components/About.js
+++ src/components/About.js
@@ -19,14 +19,11 @@ const About = () => {
         <dd>Networks Infrastructure, IT Department, Internship</dd>
         <dd>Years active: Jan - Apr 2022</dd>
         <ul className='section_about__list'>
-          <li>Maintained a 96% ticket resolution rate for assigned user-support tickets from HQ, DR and branches</li>
-          <li>Attended to network connectivity patching and troubleshooting responsibilities</li>
-          <li>Delivered on an assignment from the KCB Business Productivity team dealing in the design and working functionality of subsidiary websites</li>
-          <li>Trained for and later responsible for CISCO registry functions i.e. profile creation and maintenance</li>
-          <li>Liased with teammates in overseeing the delivery of network components to the DR site at Gigiri and the then newly-opening Imaara Mall branch site</li>
-          <li>Wrote an automation script later used as a template for the remote installation of a network compliance software scheduled for mass roll-out across the organization</li>
-          <li>Trained under various other sub-departments i.e. IT Service Center, Business Productivity, BSO, Digital Channels, Access Management etc.</li>
+          <li>Part of a team dealing in the design and working functionality of the KCB intranet subsidiary websites</li>
+          <li>Trained for and later responsible for CISCO registry functions i.e. profile creation, maintenance</li>
+          <li>Submitted a script later adopted as a template while part of a team tasked with the mass remote installation of a network compliance software across all devices in the organization</li>
         </ul>
+        <dd>... and more. Info available in my resume.</dd>
       </dl>
     </section>
   )
--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,10 +1,39 @@
+import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
 
+let showingNav = false
+
 const Navbar = () => {
+
+  const showMenuRef = React.useRef()
+  const closeMenuRef = React.useRef()
+  const menuRef = React.useRef()
+
+  const showNav = () => {
+    if(!showingNav) {
+      menuRef.current.style.top = '50%'
+      showMenuRef.current.style.visibility = 'hidden'
+      showMenuRef.current.style.opacity = '0'
+      closeMenuRef.current.style.visibility = 'visible'
+      closeMenuRef.current.style.opacity = '1'
+      showingNav = true
+    }
+    else if(showingNav) {
+      menuRef.current.style.top = '-75%'
+      showMenuRef.current.style.visibility = 'visible'
+      showMenuRef.current.style.opacity = '1'
+      closeMenuRef.current.style.visibility = 'hidden'
+      closeMenuRef.current.style.opacity = '0'
+      showingNav = false
+    }
+  }
+
   return (
     <>
       <nav>
-        <ul>
+        <svg ref={showMenuRef} className='showMenu' onClick={showNav} xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M7.5 36.35q-.7 0-1.2-.5t-.5-1.25q0-.7.5-1.175.5-.475 1.2-.475h33q.7 0 1.2.475t.5 1.225q0 .7-.5 1.2t-1.2.5Zm0-10.65q-.7 0-1.2-.5T5.8 24q0-.75.5-1.225.5-.475 1.2-.475h33q.7 0 1.2.5t.5 1.2q0 .75-.5 1.225-.5.475-1.2.475Zm0-10.65q-.7 0-1.2-.475t-.5-1.225q0-.7.5-1.2t1.2-.5h33q.7 0 1.2.5t.5 1.25q0 .7-.5 1.175-.5.475-1.2.475Z"/></svg>
+        <svg ref={closeMenuRef} className='closeMenu' onClick={showNav} xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M24 26.4 13.65 36.75q-.5.5-1.2.5t-1.2-.5q-.5-.5-.5-1.2t.5-1.2L21.6 24 11.25 13.65q-.5-.5-.5-1.2t.5-1.2q.5-.5 1.2-.5t1.2.5L24 21.6l10.35-10.35q.5-.5 1.2-.5t1.2.5q.5.5.5 1.2t-.5 1.2L26.4 24l10.35 10.35q.5.5.5 1.2t-.5 1.2q-.5.5-1.2.5t-1.2-.5Z"/></svg>
+        <ul ref={menuRef}>
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/projects'>Projects</Link></li>
         </ul>
