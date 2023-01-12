6638ed6e2f2d79da33704cb00a9d84210b7f2154
Andrew G. Kagotho <86290395+AndrewKagotho@users.noreply.github.com>
2023-01-12 23:36:03 +0300

    UI style edits

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,22 +12,25 @@ const Projects = () => {
       <p>An overview of personal works.</p>
       <div className='flex_row'>
         <article>
-          <h3>County Development Project Tracker</h3>
-          <hr/>
+          <div>
+            <h3>County Development Project Tracker</h3>
+            <button onClick={() => setProject(1)}><Link to='/project'>Go to project</Link></button>
+          </div>
           <p>An online tracking system that maintains public project records for all 47 counties in Kenya, providing project updates and insights on both county and national levels.</p>
-          <button onClick={() => setProject(1)}><Link to='/project'>Go to project</Link></button>
         </article>
         <article>
-          <h3>Portfolio</h3>
-          <hr/>
+          <div>
+            <h3>Portfolio</h3>
+            <button onClick={() => setProject(3)}><Link to='/project'>Go to project</Link></button>
+          </div>
           <p>This site. Built to showcase itself and other personal projects, providing project commentary, links and previews. Also features current unfinished projects.</p>
-          <button onClick={() => setProject(3)}><Link to='/project'>Go to project</Link></button>
         </article>
         <article>
-          <h3>Crowdfunding app</h3>
-          <hr/>
-          <p>An HTML, CSS and JavaScript challenge on Frontend Mentor further developed with an RDB backend and API integration.</p>
-          <button onClick={() => setProject(2)}><Link to='/project'>Go to project</Link></button>
+          <div>
+            <h3>Crowdfunding app</h3>
+            <button onClick={() => setProject(2)}><Link to='/project'>Go to project</Link></button>
+          </div>
+          <p>An HTML, CSS and JavaScript challenge on Frontend Mentor further developed with a simple relational database and API integration.</p>
         </article>
       </div>
       <button><Link to='/projects'>See all projects   &gt;</Link></button>
